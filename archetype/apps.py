6be3b8cbc536f25184460754af2010af09255ef6
Cash Myers <cmyers880@gmail.com>
2025-04-26 10:11:33 -0500

    feat: initialized 'apps-(tables) for later integration and development

---
+++ archetype/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArchetypeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'archetype'
---
+++ table_group/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TableGroupConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'table_group'
